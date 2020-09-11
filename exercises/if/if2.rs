55758d1ed0596dc707a021bce558663bc5da7c7f
Blaine Richardson <blainerrichardson@gmail.com>
2020-09-10 21:41:52 -0500

    feat(exercises_if/control flow) finished exercises/topic over control flow/ifs

--- exercises/if/if2.rs
+++ exercises/if/if2.rs
@@ -8,9 +8,11 @@
 
 pub fn fizz_if_foo(fizzish: &str) -> &str {
     if fizzish == "fizz" {
-        "foo"
+      "foo"
+    } else if fizzish == "fuzz" {
+      "bar"
     } else {
-        1
+      "baz"
     }
 }
 
